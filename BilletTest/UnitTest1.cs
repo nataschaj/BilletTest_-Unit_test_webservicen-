dbc3d33a045c8c495848d47b712b9af2d17a2d33
natsachaj <natascha_jakobsen@hotmail.com>
2017-09-20 12:29:01 +0200

    Add project files.

---
+++ BilletTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BilletTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var client = new ServiceReference1.Service1Client("BasicHttpBinding_IService1"))
+            {
+                //Arrange
+                //Act
+                var pris = client.GetBilPris();
+                //Assert
+                Assert.AreEqual(240, pris);
+            }
+        }
+    }
+}

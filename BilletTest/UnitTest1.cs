20a9c2d2e4e481a7f41d311806e06ea660db27d2
natsachaj <natascha_jakobsen@hotmail.com>
2017-09-20 12:33:12 +0200

    MC pris test tilføjet

--- BilletTest/UnitTest1.cs
+++ BilletTest/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace BilletTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestBilPris()
         {
             using (var client = new ServiceReference1.Service1Client("BasicHttpBinding_IService1"))
             {
@@ -16,6 +16,19 @@ namespace BilletTest
                 var pris = client.GetBilPris();
                 //Assert
                 Assert.AreEqual(240, pris);
+            }
+        }
+
+        [TestMethod]
+        public void TestMCPris()
+        {
+            using (var client = new ServiceReference1.Service1Client("BasicHttpBinding_IService1"))
+            {
+                //Arrange
+                //Act
+                var pris = client.GetMCPris();
+                //Assert
+                Assert.AreEqual(125, pris);
             }
         }
     }
